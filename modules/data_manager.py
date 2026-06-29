17b91424adb7d4b7c155d45d8a3a9ec37d3ebfff
seejoon9387-wq <seejoon9387@gmail.com>
2026-06-30 05:46:47 +0900

    Create data_manager.py

---
+++ modules/data_manager.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+class DataManager:
+    GAME_DB = "mlb_games.csv"
+    PLAYER_DB = "mlb_players.csv"
+
+    @staticmethod
+    def save_game(game_data):
+        df = pd.DataFrame([game_data])
+        df.to_csv(DataManager.GAME_DB, mode='a', header=not os.path.exists(DataManager.GAME_DB), index=False)
+
+    @staticmethod
+    def save_player_stats(player_list):
+        if not player_list: return
+        df = pd.DataFrame(player_list)
+        df.to_csv(DataManager.PLAYER_DB, mode='a', header=not os.path.exists(DataManager.PLAYER_DB), index=False)
