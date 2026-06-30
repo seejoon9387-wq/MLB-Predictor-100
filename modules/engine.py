97dde9b1408730566904e622dd4ab5002375d4b0
seejoon9387-wq <seejoon9387@gmail.com>
2026-07-01 00:37:47 +0900

    Update engine.py

--- modules/engine.py
+++ modules/engine.py
@@ -3,9 +3,15 @@ class SabermetricsEngine:
         pass
 
     def execute(self, data):
-        # 함수 내부에서 import하여 순환 참조 방지
+        # 함수 내부에서 import하여 순환 참조 방지 (Lazy Import)
         from modules.config import SCHEMA
         
-        # 분석 로직 (예시)
+        # 실제 분석 로직이 들어갈 자리입니다.
+        # 예시로 65%의 승률을 반환하도록 설정했습니다.
         prob = 65.0
-        return {'win_prob': prob, 'schema_applied': list(SCHEMA.keys())}
+        
+        return {
+            'win_prob': prob, 
+            'data_received': data,
+            'schema_applied': list(SCHEMA.keys())
+        }
