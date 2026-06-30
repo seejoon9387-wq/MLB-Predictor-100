de2f4ef4f6c58850d5518c10a5e99400732d3e2e
seejoon9387-wq <seejoon9387@gmail.com>
2026-06-30 15:11:28 +0900

    Update engine.py

--- modules/engine.py
+++ modules/engine.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.isotonic import IsotonicRegression
 
+# 모듈 1: 결정론적 데이터 품질 관리
 class DataQualityManager:
     def __init__(self, schema):
         self.schema = schema
@@ -16,6 +17,7 @@ class DataQualityManager:
             df.loc[outliers, col] = bounds['median']
         return df
 
+# 모듈 2: 확률적 예측 모델
 class ProbabilisticModel:
     def __init__(self):
         self.models = {
@@ -29,6 +31,7 @@ class ProbabilisticModel:
     def predict(self, X):
         return {name: model.predict(X) for name, model in self.models.items()}
 
+# 모듈 3: 확률 보정 (Calibration)
 class ProbabilityCalibrator:
     def __init__(self):
         self.calibrator = IsotonicRegression(out_of_bounds='clip')
@@ -36,3 +39,18 @@ class ProbabilityCalibrator:
         self.calibrator.fit(y_prob, y_true)
     def calibrate(self, y_prob):
         return self.calibrator.transform(y_prob)
+
+# 모듈 4: 확장 윈도우 백테스팅 엔진
+class Backtester:
+    def __init__(self, model_class):
+        self.model_class = model_class
+    def run_backtest(self, X, y, window_size=5):
+        results = []
+        for i in range(window_size, len(X)):
+            train_X, train_y = X.iloc[:i], y[:i]
+            test_X = X.iloc[i:i+1]
+            model = self.model_class()
+            model.fit(train_X, train_y)
+            pred = model.predict(test_X)
+            results.append(pred['median'][0])
+        return results
