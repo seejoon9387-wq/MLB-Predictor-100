c88237e3a8413ab939360488ee98048f34073fa7
seejoon9387-wq <seejoon9387@gmail.com>
2026-07-01 03:53:11 +0900

    Create tactical_engine.py

---
+++ modules/tactical_engine.py
@@ -0,0 +1,15 @@
+class TacticalEngine:
+    """
+    기존의 tactical_simulation, manager_tendency, leverage_engine, 
+    inefficiency_engine, platoon, lineup_engine 등을 통합 관리.
+    """
+    def __init__(self):
+        pass
+
+    def simulate_tactics(self, matchup_data):
+        # 기존 tactical_simulation, leverage_engine 통합
+        return {"tactical_advantage": 0.0}
+
+    def analyze_manager_impact(self, manager_profile):
+        # 기존 manager_tendency 통합
+        return {"manager_score": 0.0}
