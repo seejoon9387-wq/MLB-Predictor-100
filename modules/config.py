56d3848839e5c4194797dcc6c815029c078e725d
seejoon9387-wq <seejoon9387@gmail.com>
2026-07-01 00:36:34 +0900

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,3 +1,9 @@
+import os
+
+# 프로젝트 루트 경로 확보
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_DIR = os.path.join(BASE_DIR, "data")
+
 SCHEMA = {
     'woba_value': {'min': 0, 'max': 1.5, 'median': 0.320},
     'launch_speed': {'min': 0, 'max': 125, 'median': 88.0},
